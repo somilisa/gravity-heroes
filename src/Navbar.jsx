7daac7e56425c3d50541f7dd0d602a0f9f3932bf
Lisa <somilisa1010@gmail.com>
2023-08-03 01:09:47 +0100

    style update: mobile view and menu toggle

---
+++ src/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from './Logo.svg';
+import { FaBars } from 'react-icons/fa';
+import { links } from './data';
+import { useState } from 'react';
+
+const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+
+  return (
+    <nav>
+      <div className='nav-header'>
+        <img src={logo} alt='gravity'></img>
+        <button className='nav-toggle' onClick={() => setShowLinks(!showLinks)}>
+          <FaBars />
+        </button>
+      </div>
+      <ul className={`${showLinks ? 'show-container' : ''}`}>
+        {links.map((link) => {
+          const { id, url, text } = link;
+          return (
+            <li key={id}>
+              <a href={url}>{text}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/data.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export const links = [
+  {
+    id: 1,
+    url: '/',
+    text: 'Home',
+  },
+  {
+    id: 2,
+    url: '/apps',
+    text: 'Our Apps',
+  },{
+    id: 3,
+    url: '/clients',
+    text: 'Clients',
+  },{
+    id: 4,
+    url: '/contacts',
+    text: 'Contacts',
+  },
+]
+
